5a28c52ee21dd32ecc71a64c625ba2dfbc97f706
DAMMUCK <44718119+DAMMUCK@users.noreply.github.com>
2020-06-10 11:15:09 +0900

    Rename printStar.java to [#4]printStar.java

--- [#4]printStar.java
+++ [#4]printStar.java
@@ -9,7 +9,7 @@ public class printStar {
 		star.print();
 	}
 	public void print() {
-		System.out.print("¼ıÀÚÀÔ·Â : ");
+		System.out.print("Â¼Ã½Ã€ÃšÃ€Ã”Â·Ã‚ : ");
 		int num = scan.nextInt();
 		for(int i=1;i<num;i++) {
           for(int j=1;j<=i;j++) {
