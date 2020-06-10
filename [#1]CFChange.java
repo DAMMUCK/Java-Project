f45ce9a9a05c224dc8bc96d00843c6e840117a97
DAMMUCK <44718119+DAMMUCK@users.noreply.github.com>
2020-06-10 10:23:57 +0900

    Rename CFChange.java to [#1]CFChange.java

--- [#1]CFChange.java
+++ [#1]CFChange.java
@@ -6,13 +6,13 @@ public class CFChange {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		CFChange change = new CFChange();
-		System.out.print("¼·¾¾ : ");
+		System.out.print("Â¼Â·Â¾Â¾ : ");
 		double temp=scan.nextDouble();
 		change.changeTemp(temp);
 	}
 	public void changeTemp(double temp) {
 		double num=0;
-		System.out.print("È­¾¾ : ");
+		System.out.print("ÃˆÂ­Â¾Â¾ : ");
 		num=(temp*9/5)+32;
 		System.out.println(num);
 	}
