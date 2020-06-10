7bc6ac436058c692e62a9643c15ea74939d82de4
DAMMUCK <44718119+DAMMUCK@users.noreply.github.com>
2020-06-10 12:30:30 +0900

    Update [#4]printStar.java

--- [#4]printStar.java
+++ [#4]printStar.java
@@ -9,7 +9,7 @@ public class printStar {
 		star.print();
 	}
 	public void print() {
-		System.out.print("¼ýÀÚÀÔ·Â : ");
+		System.out.print("숫자입력 : ");
 		int num = scan.nextInt();
 		for(int i=1;i<num;i++) {
           for(int j=1;j<=i;j++) {
