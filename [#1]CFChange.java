b865165907d345dccae6f32f999d250659f31c63
DAMMUCK <44718119+DAMMUCK@users.noreply.github.com>
2020-06-10 12:31:49 +0900

    Update [#1]CFChange.java

--- [#1]CFChange.java
+++ [#1]CFChange.java
@@ -6,13 +6,13 @@ public class CFChange {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		CFChange change = new CFChange();
-		System.out.print("¼·¾¾ : ");
+		System.out.print("섭씨 : ");
 		double temp=scan.nextDouble();
 		change.changeTemp(temp);
 	}
 	public void changeTemp(double temp) {
 		double num=0;
-		System.out.print("È­¾¾ : ");
+		System.out.print("화씨 : ");
 		num=(temp*9/5)+32;
 		System.out.println(num);
 	}
